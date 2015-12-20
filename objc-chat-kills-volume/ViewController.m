2653634fb54d4cf388cfb87cf55eec4afc5a4667
Gordon Childs <gordon.childs@gmail.com>
2015-12-21 07:46:00 +1100

    http://stackoverflow.com/questions/34346209/ios-mpvolumeview-breaks-based-on-audiosession-mode

--- objc-chat-kills-volume/ViewController.m
+++ objc-chat-kills-volume/ViewController.m
@@ -6,9 +6,15 @@
 //  Copyright © 2015 Gordon Childs. All rights reserved.
 //
 
+// http://stackoverflow.com/questions/34346209/ios-mpvolumeview-breaks-based-on-audiosession-mode
+
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface ViewController ()
+
+@property (nonatomic) MPVolumeView *volumeView;
 
 @end
 
@@ -16,12 +22,30 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	self.volumeView = [[MPVolumeView alloc] init];
+	[self.view addSubview:self.volumeView];
+	
+	// Make sure the session on this thread matches.
+	AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+	[audioSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionMixWithOthers | AVAudioSessionCategoryOptionAllowBluetooth error:nil];
+	
+	// Here is the main connundrum: I want to set AVAudioSessionModeDefault so that the MPVolumeView will work,
+	// but when I do that the mic picks up the audio from the speaker and plays it back creating an unwanted feeback loop.
+	// If I set the mode to AVAudioSessionModeVoiceChat the voice processing (primarily the AEC) fixes the feeback loop,
+	// but then the MPVolumeView no longers works.  (It no longer adjusts the system volume.
+	//Question: How can I have BOTH the voice processing AND the MPVolumeView adjust the system volume?
+	if(true)
+		[audioSession setMode:AVAudioSessionModeDefault error:nil];
+	else
+		[audioSession setMode:AVAudioSessionModeVoiceChat error:nil];
+	
+	[audioSession setPreferredIOBufferDuration:0.005 error:nil];
+	[audioSession setActive:YES error:nil];
 }
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (void)viewWillLayoutSubviews {
+	self.volumeView.frame = self.view.frame;
 }
 
 @end
