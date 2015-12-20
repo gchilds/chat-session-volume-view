b97b17639c4cb81c75e95e827f3eb4f1bbb8ac56
Gordon Childs <gordon.childs@gmail.com>
2015-12-21 07:41:48 +1100

    Initial Commit

---
+++ objc-chat-kills-volume/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objc-chat-kills-volume
+//
+//  Created by Gordon Childs on 21/12/2015.
+//  Copyright © 2015 Gordon Childs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objc-chat-kills-volume/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objc-chat-kills-volume
+//
+//  Created by Gordon Childs on 21/12/2015.
+//  Copyright © 2015 Gordon Childs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
